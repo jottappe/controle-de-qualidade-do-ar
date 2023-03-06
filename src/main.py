787a4a15da195f4526120e1ebc6543e3efbbcd1d
João Pedro <candido.jp@hotmail.com>
2023-03-06 19:38:51 -0300

    COrreção inputs.

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ print('Entre com a leitura dos valores de parâmetros:')
 
 particulasInalaveis = input('Partículas inaláveis (24h)')
 particulasInalaveisFinas = input('Partículas inaláveis finas (24h)')
-ozonio = input('Partículas inaláveis finas (8h)')
-monoxidoCarbono = input('Partículas inaláveis finas (8h)')
-dioxidoNitrogenio = input('Partículas inaláveis finas (8h)')
-dioxidoEnxofre = input('Partículas inaláveis finas (8h)')
+ozonio = input('Ozônio (8h)')
+monoxidoCarbono = input('Monóxido de Carbono (8h)')
+dioxidoNitrogenio = input('Dióxido de Nitrogênio (8h)')
+dioxidoEnxofre = input('Dióxido de Enxofre (8h)')
 
 qualidadeMP10 = 0
 qualidadeMP25 = 0
@@ -37,10 +37,51 @@ elif particulasInalaveis > 75 and particulasInalaveis <= 125:
     qualidadeMP25 = 4
 else:
     qualidadeMP25 = 5
+
 # Verificar qualidade para O3:
+if particulasInalaveis > 0 and particulasInalaveis <= 100:
+    qualidadeOzonio = 1
+elif particulasInalaveis > 100 and particulasInalaveis <= 130:
+    qualidadeOzonio = 2
+elif particulasInalaveis > 130 and particulasInalaveis <= 160:
+    qualidadeOzonio = 3
+elif particulasInalaveis > 160 and particulasInalaveis <= 200:
+    qualidadeOzonio = 4
+else:
+    qualidadeOzonio = 5
 
 # Verificar qualidade para CO:
+if particulasInalaveis > 0 and particulasInalaveis <= 9:
+    qualidadeMonoxidoCarbono = 1
+elif particulasInalaveis > 9 and particulasInalaveis <= 11:
+    qualidadeMonoxidoCarbono = 2
+elif particulasInalaveis > 11 and particulasInalaveis <= 13:
+    qualidadeMonoxidoCarbono = 3
+elif particulasInalaveis > 13 and particulasInalaveis <= 15:
+    qualidadeMonoxidoCarbono = 4
+else:
+    qualidadeMonoxidoCarbono = 5
 
 # Verificar qualidade para NO2:
+if particulasInalaveis > 0 and particulasInalaveis <= 200:
+    qualidadeDioxidoNitrogenio = 1
+elif particulasInalaveis > 200 and particulasInalaveis <= 240:
+    qualidadeDioxidoNitrogenio = 2
+elif particulasInalaveis > 240 and particulasInalaveis <= 320:
+    qualidadeDioxidoNitrogenio = 3
+elif particulasInalaveis > 320 and particulasInalaveis <= 1130:
+    qualidadeDioxidoNitrogenio = 4
+else:
+    qualidadeDioxidoNitrogenio = 5
 
 # Verificar qualidade para SO2:
+if particulasInalaveis > 0 and particulasInalaveis <= 20:
+    qualidadeDioxidoEnxofre = 1
+elif particulasInalaveis > 20 and particulasInalaveis <= 40:
+    qualidadeDioxidoEnxofre = 2
+elif particulasInalaveis > 40 and particulasInalaveis <= 365:
+    qualidadeDioxidoEnxofre = 3
+elif particulasInalaveis > 365 and particulasInalaveis <= 800:
+    qualidadeDioxidoEnxofre = 4
+else:
+    qualidadeDioxidoEnxofre = 5
